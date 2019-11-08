277d75f595cdc1b4e4bf078f9333c601c8411ce1
Maria Tudose <maria.tudose@aalto.fi>
2019-11-08 15:42:52 +0200

    4.4: apufunktioita ja yksikkötestejä, step2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,8 +1,19 @@
 const listHelper = require('../utils/list_helper')
+const { blogs } = require('./dummyBlogs')
 
 test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
+  const result = listHelper.dummy([])
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes([blogs[0]])
+    expect(result).toBe(7)
+  })
+
+  test('correctly sums the likes for all blogs', () => {
+    const result = listHelper.totalLikes(blogs)
+    expect(result).toBe(36)
+  })
 })
