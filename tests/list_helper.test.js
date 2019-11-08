9efc89197d06f2d7f07986e55b2effb8d78a3094
Maria Tudose <maria.tudose@aalto.fi>
2019-11-08 16:44:59 +0200

    4.5*: apufunktioita ja yksikkötestejä, step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -7,13 +7,35 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
+
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes([blogs[0]])
     expect(result).toBe(7)
   })
 
-  test('correctly sums the likes for all blogs', () => {
+  test('equals the sum of the likes for all blogs', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty list returns 0', () => {
+    const result = listHelper.favoriteBlog([])
+    expect(result).toEqual(0)
+  })
+
+  test('is the only blog even if it has no likes', () => {
+    const result = listHelper.favoriteBlog([blogs[4]])
+    expect(result._id).toBe("5a422ba71b54a676234d17fb")
+  })
+
+  test('is the blog with most likes', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    expect(result._id).toBe("5a422b3a1b54a676234d17f9")
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce((a,b) => a + b, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog, 0)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
