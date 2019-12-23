465204516821d4656588efee215067a4e83081d0
Maria Tudose <maria.tudose@aalto.fi>
2019-12-23 14:37:58 +0200

    added middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 
 const mongoUrl = config.MONGODB_URI
@@ -12,7 +13,11 @@ mongoose.connect(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
