901129d1c30d0b330f240f7af924aeab593e0547
Maria Tudose <maria.tudose@aalto.fi>
2019-12-23 15:15:12 +0200

    mongo fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const config = require('./utils/config')
 
 const mongoUrl = config.MONGODB_URI
 
-mongoose.connect(mongoUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(mongoUrl, { useUnifiedTopology: true, useFindAndModify: false, useNewUrlParser: true })
 
 app.use(cors())
 app.use(bodyParser.json())
